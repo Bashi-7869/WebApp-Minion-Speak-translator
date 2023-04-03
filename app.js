3867c0363adaed69e59960fb37a3b2b10db7d1bd
Bashi-7869 <121685218+Bashi-7869@users.noreply.github.com>
2023-04-03 12:05:03 +0530

    adding eventlistener

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-let userName = prompt("Enter User Name")
-let wellcomeMessage = "wellcome to java script " + userName
-alert(wellcomeMessage)
+// let userName = prompt("Enter User Name")
+// let wellcomeMessage = "wellcome to java script " + userName
+// alert(wellcomeMessage)
+
+var btnTranslat = document.querySelector("#btn-translat");
+
+
+btnTranslat.addEventListener("click",clickHandler)
+
+function clickHandler(){
+    console.log("clickd!");
+}
