9393be7676ddb21ec519df3a21cbd8b2d0138d18
Bashi-7869 <121685218+Bashi-7869@users.noreply.github.com>
2023-04-03 12:25:19 +0530

    taking input form userend

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 // let wellcomeMessage = "wellcome to java script " + userName
 // alert(wellcomeMessage)
 
+var textInput =document.querySelector("#text-input")
 var btnTranslat = document.querySelector("#btn-translat");
-
+console.log(textInput);
 
 btnTranslat.addEventListener("click",clickHandler)
 
 function clickHandler(){
-    console.log("clickd!");
+    // console.log("clickd!");
+    console.log("input",textInput.value);
 }
